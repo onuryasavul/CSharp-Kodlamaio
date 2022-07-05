76770542bd901b34fd67716c771429aeef0dd951
ONUR YASAVUL <onur.yasavul@std.yildiz.edu.tr>
2022-07-05 15:12:23 +0300

    4.ders sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Onur", "Mert", "Selçuk" };
+
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //isimler[3] = Console.ReadLine();
+            //Console.WriteLine(isimler[3]); Hata verir!!!
+
+
+            List<string> isimler2 = new List<string> {"Onur", "Mert", "Selçuk" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("Mehmet");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list1 = new MyList<string>();
+            list1.Add("Foo");
+            
+
+        }
+    }
+}
